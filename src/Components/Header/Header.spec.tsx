7f107c1005f4658303d1cfa7639dabe040f6d409
Cole Schneider <colekschneider@gmail.com>
2019-04-12 09:32:15 -0700

    more coverage

--- src/Components/Header/Header.spec.tsx
+++ src/Components/Header/Header.spec.tsx
@@ -22,4 +22,32 @@ describe('Header', () => {
     wrapper.update();
     expect(wrapper.find('.is-open')).toBeDefined();
   });
+  it('registers the event listener on mount', () => {
+    const windowSpy = jest.spyOn(window, 'addEventListener');
+    const wrapper = mount(<Header />);
+    expect(windowSpy).toBeCalled();
+  });
+  it('unregister the event listener on unmount', () => {
+    const windowSpy = jest.spyOn(window, 'removeEventListener');
+    const wrapper = mount(<Header />);
+    wrapper.unmount();
+    expect(windowSpy).toBeCalled();
+  });
+  it('calls setHeaderScroll as it scrolls', () => {
+    const setHeaderScroll = jest.fn(isHome => isHome);
+    const wrapper = shallow(<Header setHeaderScroll={setHeaderScroll} />);
+    wrapper.instance().handleSetActive('home');
+    expect(setHeaderScroll).toBeCalledWith(false);
+  });
+  it('calls setHeaderScroll as it scrolls with false', () => {
+    const setHeaderScroll = jest.fn(isHome => isHome);
+    const wrapper = shallow(<Header setHeaderScroll={setHeaderScroll} />);
+    wrapper.instance().handleSetActive('mission');
+    expect(setHeaderScroll).toBeCalledWith(true);
+  });
+  it('sets state of isDropdown', () => {
+    const wrapper = shallow(<Header />);
+    wrapper.instance().toggleDropdown();
+    expect(wrapper.state().isDropdown).toBe(true);
+  });
 });
