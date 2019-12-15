c5ca2032ded66768d188f819cfbe2b652c804676
Cole Schneider <colekschneider@gmail.com>
2019-12-15 15:31:06 -0800

    try again for the photo

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import 'raf/polyfill';
 import { BrowserRouter, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { Helmet } from 'react-helmet';
-import BrochureTwo from 'Images/brochure_two.png';
+import Beach from 'Images/beach.jpg';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -19,14 +19,14 @@ ReactDOM.render(
       <meta property="og:description" content="The Future of Oxnard's Senior Center is Bright" />
       <meta property="og:url" content="https://orcaseniors.org/" />
       <meta property="og:site_name" content="orcaseniors" />
-      <meta property="og:image" content={BrochureTwo} />
-      <meta property="og:image:secure_url" content={BrochureTwo} />
-      <meta property="og:image:width" content="1056" />
-      <meta property="og:image:height" content="816" />
+      <meta property="og:image" content={Beach} />
+      <meta property="og:image:secure_url" content={Beach} />
+      <meta property="og:image:width" content="1536" />
+      <meta property="og:image:height" content="1024" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:description" content="The Future of Oxnard's Senior Center is Bright" />
       <meta name="twitter:title" content="Oxnard Recreation Center for Adults & Seniors" />
-      <meta name="twitter:image" content={BrochureTwo} />
+      <meta name="twitter:image" content={Beach} />
     </Helmet>
     <Route component={App} />
   </BrowserRouter>,
